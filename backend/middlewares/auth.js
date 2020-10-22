c9f22cc28ab86d3b7ce54243f82ff6e41bf04822
Raene <bob.salau@gmail.com>
2020-10-22 11:43:36 +0100

    added SA functionality and route

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -19,6 +19,18 @@ exports.loginRequired = async (ctx, next) => {
 
 exports.isAdmin = async function (ctx, next) {
     try {
+        if(ctx.state.user.role != 1 || ctx.state.user.role != 2){
+            throw new Error('not admin');
+        }
+        await next();
+    } catch (err) {
+        ctx.body = { 'error': err, 'message': err.message };
+        ctx.status = 401;
+    }
+};
+
+exports.isSuperAdmin = async function (ctx, next) {
+    try {
         if(ctx.state.user.role != 1){
             throw new Error('not admin');
         }
