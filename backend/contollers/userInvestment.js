245e651c1d042ca19ad713e4acfa79e188cb4174
Raene <bob.salau@gmail.com>
2020-10-30 05:23:51 +0100

    fixed route to use querystring

--- backend/contollers/userInvestment.js
+++ backend/contollers/userInvestment.js
@@ -6,10 +6,9 @@ exports.getInvestments = function (con) {
         try {
             const  id = ctx.params.id;
             const valueType = ctx.query['type'];
-            console.log(valueType);
             const userInvestments = new UserInvestmentModel({},con,'userInvestments');
 
-            let payload = await userInvestments.get(id, 'id');
+            let payload = await userInvestments.get(id, valueType);
 
             ctx.body = {data: payload, success:true}
             ctx.status = 200;
