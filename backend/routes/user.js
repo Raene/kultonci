61de68ec3a2cf964cd0828de62520228d7415809
mysterymosim <okimmosimiracle@gmail.com>
2020-10-30 04:08:41 +0000

    processed user payment flow

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,11 @@
 let controller = require('../contollers/user');
+let userInvestment = require('../contollers/userInvestment');
 
-exports.users = function(router,con) {
+exports.users = function(router, con) {
     const route = new router({
         prefix: '/server/user'
-    }); 
+    });
 
-    return route.post('/createDeposit',controller.createDeposit(con));
+    return route.post('/createDeposit', controller.createDeposit(con))
+        .get('/investments/:id', userInvestment.getInvestments(con));
 }
