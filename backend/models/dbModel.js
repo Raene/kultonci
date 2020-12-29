7b98a6470c8661d214bac17fe7bd59e455cc96d2
raene <bob.salau@gmail.com>
2020-12-29 17:59:47 +0100

    refactored db conn to use and release pools

--- backend/models/dbModel.js
+++ backend/models/dbModel.js
@@ -1,165 +1,286 @@
 'use strict';
-let Db = function (con,TableName) {
-        this.con = con;
-        this.TableName = TableName;
+let Db = function (con, TableName) {
+    this.con = con;
+    this.TableName = TableName;
 };
 
-Db.prototype.insertToTable = function (obj) {
-    return new Promise( (resolve, reject) => {
+Db.prototype.makeQuery = async function (queryfunction) {
+    const connection = await new Promise((resolve, reject) => {
+        this.con.getConnection(function (err, connection) {
+            if (err) {
+                reject(err);
+            }
+            connection.on('error', function (err) {
+                reject(err);
+            });
+            resolve(connection)
+        })
+    });
+    try {
+        return await queryfunction(connection)
+    } catch (error) {
+        throw new Error(error)
+    } finally {
+        connection.release()
+    }
+}
+
+
+Db.prototype.insertToTable = async function (obj) {
+    try {
         let sql = `INSERT INTO ${this.TableName} SET ?`;
-        this.con.query(sql,obj, function (err,results) {
-            if(err) reject(err);
-            resolve(results);  
+        let result = await this.makeQuery(async connection => {
+            return await new Promise((resolve, reject) => {
+                connection.query(sql, obj, function (err, results) {
+                    if (err) reject(err);
+                    resolve(results);
+                });
+            })
         });
-    });
+        console.log(result);
+        return result;
+    } catch (error) {
+        throw new Error(error)
+    }
 };
 
-Db.prototype.getByField = function (value,valueType){
-    return new Promise((resolve, reject) => {
+Db.prototype.getByField = async function (value, valueType) {
+    try {
         let sql = `SELECT * FROM ${this.TableName} WHERE ${valueType} = ?`;
-        this.con.query(sql,value, function (err,results) {
-            if(err) reject(err);
-            resolve(results);
-        });
-    });
+        let result = await this.makeQuery(async connection => {
+            return await new Promise((resolve, reject) => {
+                connection.query(sql, value, function (err, results) {
+                    if (err) reject(err);
+                    resolve(results);
+                });
+            });
+        })
+        return result;
+    } catch (error) {
+        throw new Error(error)
+    }
 };
 
-Db.prototype.getByFieldUser = function (value,valueType){
-    return new Promise((resolve, reject) => {
+Db.prototype.getByFieldUser = async function (value, valueType) {
+    try {
         let sql = `SELECT id,name,email,kyc_id,created_at,updated_at,deleted_at,role,verified,referral,referee_id FROM ${this.TableName} WHERE ${valueType} = ?`;
-        this.con.query(sql,value, function (err,results) {
-            if(err) reject(err);
-            resolve(results);
-        });
-    });
+        let result = await this.makeQuery(async connection => {
+            return new Promise((resolve, reject) => {
+                connection.query(sql, value, function (err, results) {
+                    if (err) reject(err);
+                    resolve(results);
+                });
+            });
+        })
+        return result;
+    } catch (error) {
+        throw new Error(error)
+    }
 };
 
-Db.prototype.getByFieldLatest = function (value,valueType){
-    return new Promise((resolve, reject) => {
+Db.prototype.getByFieldLatest = async function (value, valueType) {
+    try {
         let sql = `SELECT * FROM ${this.TableName} WHERE ${valueType} = ?  ORDER BY id DESC LIMIT 1`;
-        this.con.query(sql,value, function (err,results) {
-            if(err) reject(err);
-            resolve(results);
-        });
-    });
+        let result = await this.makeQuery(async connection => {
+            return new Promise((resolve, reject) => {
+                connection.query(sql, value, function (err, results) {
+                    if (err) reject(err);
+                    resolve(results);
+                });
+            });
+        })
+        return result;
+    } catch (error) {
+        throw new Error(error)
+    }
 };
 
-Db.prototype.getAllDb = function () {
-    return new Promise((resolve,reject) => {
+Db.prototype.getAllDb = async function () {
+    try {
         let sql = `SELECT * FROM ${this.TableName}`;
-        this.con.query(sql, function (err,results) {
-            if(err) reject(err);
-            resolve(results);
-        });
-    });
+        let result = await this.makeQuery(async connection => {
+            return new Promise((resolve, reject) => {
+                connection.query(sql, function (err, results) {
+                    if (err) reject(err);
+                    resolve(results);
+                });
+            });
+        })
+        return result;
+    } catch (error) {
+        throw new Error(error);
+    }
 }
 
-Db.prototype.dbDelete = function (value,valueType) {
-    return new Promise((resolve,reject) => {
+Db.prototype.dbDelete = async function (value, valueType) {
+    try {
         let sql = `DELETE FROM ${this.TableName} WHERE ${valueType} = ?`;
-        this.con.query(sql,value, function (err,results) {
-            if(err) reject(err);
-            resolve(results);
+        let result = await this.makeQuery(async connection => {
+            return new Promise((resolve, reject) => {
+                connection.query(sql, value, function (err, results) {
+                    if (err) reject(err);
+                    resolve(results);
+                });
+            })
+        })
+        return result;
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+Db.prototype.getLatest = async function () {
+    try {
+        let result = await this.makeQuery(async connection => {
+            return new Promise((resolve, reject) => {
+                connection.query(`SELECT * FROM ${this.TableName} ORDER BY id DESC LIMIT 1`, function (err, results) {
+                    if (err) reject(err);
+                    resolve(results);
+                });
+            })
+        })
+        return result;
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+Db.prototype.getJoin = async function () {
+    try {
+        let sql = `SELECT *,packageLevels.name as PackageName,packageLevels.id as PackageId FROM packageLevels INNER JOIN investmentPKG ON packageLevels.investmentPkg_id = investmentPKG.id`;
+        let result = await this.makeQuery(async connection => {
+            return new Promise((resolve, reject) => {
+                connection.query(sql, function (err, result) {
+                    if (err) reject(err);
+                    resolve(result);
+                })
+            })
+        })
+        return result
+    } catch (error) {
+        throw new Error(error)
+    };
+}
+
+Db.prototype.getJoinWhere = async function (value, valueType) {
+    try {
+        let sql = `SELECT *,packageLevels.name as PackageName,packageLevels.id as PackageId FROM packageLevels INNER JOIN investmentPKG ON packageLevels.investmentPkg_id = investmentPKG.id WHERE ${valueType} = ?`;
+        let result = await this.makeQuery(async connection => {
+            return new Promise((resolve, reject) => {
+                connection.query(sql, value, function (err, result) {
+                    if (err) reject(err);
+                    resolve(result);
+                })
+            });
         });
-    })
+        return result
+    } catch (error) {
+        throw new Error(error)
+    }
 }
 
-Db.prototype.getLatest = function () {
-    return new Promise((resolve,reject)=> {
-        this.con.query(`SELECT * FROM ${this.TableName} ORDER BY id DESC LIMIT 1`,function (err,results) {
-            if (err) reject(err);
-            resolve(results);
-        });
-    })
+Db.prototype.getUsersJoin =async function () {
+    try {
+        let sql = `SELECT user.name as name,user.id as userId,user.email as userEmail,user.kyc_id as kycId, user.role as userRole, user.verified as userVerified, user.referee_id as referee_id,user.referral as  referral_code,user.created_at as userCreatedAt, kyc.kycId as kycPath FROM user INNER JOIN kyc ON user.kyc_id = kyc.id`;
+        let result = await this.makeQuery(async connection => {
+            return new Promise((resolve, reject) => {
+                connection.query(sql, function (err, result) {
+                    if (err) reject(err);
+                    resolve(result);
+                })
+            });
+        })
+        return result
+    } catch (error) {
+        throw new Error(error)
+    }
 }
 
-Db.prototype.getJoin = function () {
-    return new Promise((resolve,reject)=>{
-        let sql= `SELECT *,packageLevels.name as PackageName,packageLevels.id as PackageId FROM packageLevels INNER JOIN investmentPKG ON packageLevels.investmentPkg_id = investmentPKG.id`;
-        this.con.query(sql,function (err,result) {
-        if(err) reject(err);
-        resolve(result);
-        }) 
-    });
+Db.prototype.getUserJoin = async function (value, valueType) {
+    try {
+        let sql = `SELECT user.name as name,user.id as userId,user.email as userEmail,user.kyc_id as kycId, user.role as userRole, user.verified as userVerified,user.referee_id as referee_id, user.referral as  referral_code,user.created_at as userCreatedAt, kyc.kycId as kycPath FROM user INNER JOIN kyc ON user.kyc_id = kyc.id WHERE ${valueType} = ?`;
+        let result = await this.makeQuery(async connection => {
+            return new Promise((resolve, reject) => {
+                connection.query(sql, value, function (err, result) {
+                    if (err) reject(err);
+                    resolve(result);
+                })
+            });
+        })
+        return result;
+    } catch (error) {
+        throw new Error(error)
+    }
 }
 
-Db.prototype.getJoinWhere = function (value,valueType) {
-    return new Promise((resolve,reject)=>{
-        let sql= `SELECT *,packageLevels.name as PackageName,packageLevels.id as PackageId FROM packageLevels INNER JOIN investmentPKG ON packageLevels.investmentPkg_id = investmentPKG.id WHERE ${valueType} = ?`;
-        this.con.query(sql,value,function (err,result) {
-        if(err) reject(err);
-        resolve(result);
-        }) 
-    });
-}
-
-Db.prototype.getUsersJoin = function () {
-    return new Promise((resolve,reject)=>{
-        let sql= `SELECT user.name as name,user.id as userId,user.email as userEmail,user.kyc_id as kycId, user.role as userRole, user.verified as userVerified, user.referee_id as referee_id,user.referral as  referral_code,user.created_at as userCreatedAt, kyc.kycId as kycPath FROM user INNER JOIN kyc ON user.kyc_id = kyc.id`;
-        this.con.query(sql,function (err,result) {
-        if(err) reject(err);
-        resolve(result);
-        }) 
-    });
-}
-
-Db.prototype.getUserJoin = function (value,valueType) {
-    return new Promise((resolve,reject)=>{
-        let sql= `SELECT user.name as name,user.id as userId,user.email as userEmail,user.kyc_id as kycId, user.role as userRole, user.verified as userVerified,user.referee_id as referee_id, user.referral as  referral_code,user.created_at as userCreatedAt, kyc.kycId as kycPath FROM user INNER JOIN kyc ON user.kyc_id = kyc.id WHERE ${valueType} = ?`;
-        this.con.query(sql,value,function (err,result) {
-        if(err) reject(err);
-        resolve(result);
-        }) 
-    });
-}
-
-Db.prototype.updateDb = function (valueType,whereType,value,whereValue) {
-    return new Promise((resolve,reject)=>{
+Db.prototype.updateDb = async function (valueType, whereType, value, whereValue) {
+    try {
         let sql = `UPDATE ${this.TableName} SET ${valueType} = ? WHERE ${whereType} = ?`;
-
-        let arr = [value,whereValue]
-        this.con.query(sql,arr, function (err,results) {
-            if(err) reject(err);
-            resolve(results);
+        let result = await this.makeQuery(async connection => {
+            return new Promise((resolve, reject) => {
+                let arr = [value, whereValue]
+                connection.query(sql, arr, function (err, results) {
+                    if (err) reject(err);
+                    resolve(results);
+                })
+            })
         })
-    })
+        return result;
+    } catch (error) {
+        throw new Error(error)
+    }
 }
 
-Db.prototype.updateDbDynamic = function (obj,whereValue,whereType) {
-    return new Promise((resolve,reject)=>{
+Db.prototype.updateDbDynamic = async function (obj, whereValue, whereType) {
+    try {
         let sql = `UPDATE ${this.TableName} SET ${Object.keys(obj).map(key => `${key} = ?`).join(", ")}  WHERE ${whereType} = ?`;
-        let arr = [...Object.values(obj),whereValue]
-        this.con.query(sql,arr, function (err,results) {
-            if(err) reject(err);
-            resolve(results);
+        let result = await this.makeQuery(async connection => {
+            return new Promise((resolve, reject) => {
+                let arr = [...Object.values(obj), whereValue]
+                connection.query(sql, arr, function (err, results) {
+                    if (err) reject(err);
+                    resolve(results);
+                })
+            })
         })
-    })
+        return result;
+    } catch (error) {
+        throw new Error(error)
+    }
 }
 
-Db.prototype.getPaymentsJoin = function() {
-    return new Promise((resolve,reject)=>{
+Db.prototype.getPaymentsJoin = async function () {
+    try {
         let sql = `SELECT payment.id as paymentId,
-                   payment.paymentImg,
-                   payment.user_id as userId,
-                   payment.tmpDeposit_id as tmpDepositId, 
-                   tmpDeposit.deposit as Deposit, 
-                   tmpDeposit.pkgName as PackageName, 
-                   tmpDeposit.pkgLevel as PackageLevel, 
-                   user.name as name, 
-                   user.id as userId FROM payment INNER JOIN user 
-                   ON payment.user_id = user.id 
-                   INNER JOIN tmpDeposit 
-                   ON payment.tmpDeposit_id = tmpDeposit.id`;
-        this.con.query(sql,function(err,results) {
-            if(err) reject(err);
-            resolve(results);
-        });
-    })
+        payment.paymentImg,
+        payment.user_id as userId,
+        payment.tmpDeposit_id as tmpDepositId, 
+        tmpDeposit.deposit as Deposit, 
+        tmpDeposit.pkgName as PackageName, 
+        tmpDeposit.pkgLevel as PackageLevel, 
+        user.name as name, 
+        user.id as userId FROM payment INNER JOIN user 
+        ON payment.user_id = user.id 
+        INNER JOIN tmpDeposit 
+        ON payment.tmpDeposit_id = tmpDeposit.id`;
+
+        let result = await this.makeQuery(async connection => {
+            return new Promise((resolve, reject) => {
+                connection.query(sql, function (err, results) {
+                    if (err) reject(err);
+                    resolve(results);
+                });
+            })
+        })
+        return result;
+    } catch (error) {
+        throw new Error(error)
+    }
 };
 
 
-Db.prototype.getPaymentJoin = function (value,valueType) {
-    return new Promise((resolve,reject)=>{
+Db.prototype.getPaymentJoin = async function (value, valueType) {
+    try {
         let sql = `SELECT payment.id as paymentId,
                    payment.paymentImg,
                    payment.user_id as userId,
@@ -175,12 +296,19 @@ Db.prototype.getPaymentJoin = function (value,valueType) {
                    INNER JOIN tmpDeposit 
                    ON payment.tmpDeposit_id = tmpDeposit.id
                    WHERE ${valueType} = ?`;
-                   this.con.query(sql,value,function(err,result) {
-                       if(err) reject(err);
-                       resolve(result);
-                   })                   
-    })
+
+        let result = await this.makeQuery(async connection => {
+            return new Promise((resolve, reject) => {
+                connection.query(sql, value, function (err, result) {
+                    if (err) reject(err);
+                    resolve(result);
+                })
+            })
+        })
+        return result
+    } catch (error) {
+        throw new Error(error)
+    }
 }
 module.exports = Db;
 
- 
