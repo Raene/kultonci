9010f1e034f7708050fe5132e930c278ee54efc2
mysterymosi <okimmosimiracle@gmail.com>
2021-09-21 09:15:50 +0100

    added text

--- frontend/src/views/InvestmentPackages.vue
+++ frontend/src/views/InvestmentPackages.vue
@@ -73,10 +73,15 @@
           <div class="row">
             <div class="col-lg-12 offset-lg-1 py-3 mt-lg-5 wow fadeInUp">
               <h1 class="mb-4">Risk Disclaimer</h1>
-              <p class="mb-4">Services include products that are traded on margin and carry a risk of losses. Please ensure that you fully understand the risks involved. In light of any risk involved or encountered during trading, Lunar Alliance Investments guarantees a 90% refund of capital although, fortunately, we have recorded zero losses since our inception in 2015.
+              <p>Services include products that are traded on margin and carry a risk of losses. Please ensure that you fully understand the risks involved. In light of any risk involved or encountered during trading, Lunar Alliance Investments guarantees a 90% refund of capital although, fortunately, we have recorded zero losses since our inception in 2015.
                 Lunar Alliance Investments is issued by Lunar Alliance Investments Branch ("Issuer"). By
                 submitting this application, you are requesting Issuer ("we", "us", or "our") to establish an Lunar Alliance Investments account (the "Account") in your name, and you are agreeing to the following terms and conditions. If approved, your Account will be governed by Lunar Alliance Investments Customer Agreement provided to you at Account opening. The rates and terms associated with your Account are subject to change at any time, subject to applicable law.</p>
+                <p class="small mb-0">
+                All investment packages will run for 15 weeks from the day recorded of each investors first profit before renewal.
+              </p>
             </div>
+
+              
           </div>
         </div>
       </div>
